fad439e727e7aaf443076218a5a8ca5e270c979d
ValentynaVK <valia2526.vk@gmail.com>
2025-05-06 10:37:28 +0300

    path

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { deleteContact } from "../../redux/contactsSlice";
+import { deleteContact } from "../../redux/slices/contactsSlice";
 import css from "./Contact.module.css";
 import { IoCall, IoPerson } from "react-icons/io5";
 
